fca5d910580595882a4abc198c2996a63df08a37
duongle01555 <duongle01555@gmail.com>
2025-07-05 00:49:46 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/app/app.js
@@ -0,0 +1,70 @@
+export default {
+	init () {
+		data.carItems = appsmith.store.carItems || data.carData;
+		data.favoriteCarItems = data.carItems.filter((carItem) => carItem.favorite == true);
+		data.activeCar = appsmith.store.activeCar || 1;
+		data.carScore = appsmith.store.carScore || "not available";
+		data.bestCar = appsmith.store.bestCar || {name: "Not available", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtqlFt5SZfpRi-jda556o-QKgpYfj5Cy24jg&s"};
+	},
+	updateCar (id, update) {
+		data.carItems = [...data.carItems[id], ...update];
+		storeValue('carItems', data.carItems);
+		this.init();
+	},
+	addFavorite (id) {
+		if (data.carItems[id-1].favorite == false) {
+			data.carItems[id-1].favorite = true;
+			storeValue('carItems', data.carItems);
+			this.init();
+		}
+	},
+	editNote (id, note) {
+		data.carItems[id].note = note;
+		storeValue('carItems', data.carItems);
+		this.init();
+	},
+	removeFavorite (id) {
+		if (data.carItems[id-1].favorite == true) {
+			data.carItems[id-1].favorite = false;
+			storeValue('carItems', data.carItems);
+			this.init();
+		}
+	},
+	calculateBestCar () {
+		const toyotaRate = 90;
+		const huyndaiRate = 80;
+		const mazdaRate = 70;
+		const idealPrice = 6000;
+		function brandConverter (carItem) {
+			var brand;
+			if (carItem.brand == "Toyota") {brand = toyotaRate}
+			else if (carItem.brand == "Mazda") {brand = mazdaRate}
+			else {brand = huyndaiRate};
+			return brand;
+		}
+		function priceConverter (carItem) {
+			var priceScore;
+			if (carItem.price <= idealPrice) {
+				priceScore = (idealPrice - carItem.price) / idealPrice * 100;
+			}
+			else {
+				priceScore = Math.max((0, 100 - (carItem.price - idealPrice) / 100));
+			}
+			return priceScore;
+		}
+		var i = 0;
+		var bestScore = 0;
+		while (i < data.favoriteCarItems.length) {
+			var carItem = data.favoriteCarItems[i];
+			var Score = 0.6*priceConverter(carItem) + 0.4*brandConverter(carItem);
+			if (Score > bestScore) {
+				bestScore = Score;
+				data.bestCar = data.favoriteCarItems[i];
+			};
+			i += 1;
+		}
+		storeValue('carScore', String(bestScore));
+		storeValue('bestCar', data.bestCar);
+		this.init();
+	}
+}
---
+++ pages/Page1/jsobjects/data/data.js
@@ -0,0 +1,18 @@
+export default {
+	carItems: undefined,
+	favoriteCarItems: undefined,
+	activeCar: undefined,
+	bestCar: undefined,
+	carScore: undefined,
+	carData: [
+		{
+			id: "1", name: "Toyota Cambri", price: 3500, buildYear: "2003", img: "https://i.imgur.com/0u5TLhb.jpeg", brand: "Toyota", note: "very reliable", favorite: false
+		},
+		{ 
+			id: "2", name: "Mazda 2", price: 5000, buildYear: "2014", img: "https://img.pcauto.com/model/images/touPic/my/Mazda-2-Sedan_134.png", brand: "Mazda", note: "looks super cool", favorite: false
+		},
+		{
+			id: "3", name: "Huyndai i30", price: 5500, buildYear: "2010", img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/2018_Hyundai_i30_SE_Nav_T-GDi_1.3_Front.jpg/330px-2018_Hyundai_i30_SE_Nav_T-GDi_1.3_Front.jpg", brand: "Huyndai", note: "my friend Grace drives this car", favorite: true
+		}
+	]
+}
